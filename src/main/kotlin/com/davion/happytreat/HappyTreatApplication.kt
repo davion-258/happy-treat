77238d7cdb0fc516f37de919455ee26e11d2f11e
davion258 <hoangnam.itus@gmail.com>
2024-03-14 20:41:10 +0700

    Init Backend and Frontend app

---
+++ src/main/kotlin/com/davion/happytreat/HappyTreatApplication.kt
@@ -0,0 +1,11 @@
+package com.davion.happytreat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HappyTreatApplication
+
+fun main(args: Array<String>) {
+	runApplication<HappyTreatApplication>(*args)
+}
---
+++ src/main/kotlin/com/davion/happytreat/controller/ReceiptController.kt
@@ -0,0 +1,15 @@
+package com.davion.happytreat.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class ReceiptController(
+) {
+    @GetMapping("/list-receipt")
+    fun listReceipt(): List<String> {
+        return listOf("Receipt")
+    }
+}
